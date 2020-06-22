c6792f430897c6190114c6174fa586c5e5088bce
Roy <roywu2018@gmail.com>
2020-06-22 16:46:51 -0400

    adding landmark

--- _devel-Roy/main.py
+++ _devel-Roy/main.py
@@ -3,9 +3,8 @@ Author:      Roy Wu
 Description: Define a small 10x10 square world, a measurement range that is 
              half that of the world and small values for motion and measurement noise
 """
-
 import matplotlib.pyplot as plt
-import robot                       #* custom library
+from robot   import Robot          #* custom library
 from helpers import display_world  #* custom library
 
 #*----- ----- -----
@@ -17,19 +16,31 @@ motion_noise       = 0.2     # noise in robot motion
 measurement_noise  = 0.2     # noise in the measurements
 
 #*instantiate a robot, r
-r = robot.Robot(world_size, measurement_range, motion_noise, measurement_noise)
-
-#*print out the location of r
-print(r)
+r = Robot(world_size, measurement_range, motion_noise, measurement_noise)
+print(r)  #* print out the location of r
 
 
 #*----- ----- -----
 #*   Visualize The World
 #*----- ----- -----
+plt.rcParams["figure.figsize"] = (10, 10)  #*define figure size
 
-
-plt.rcParams["figure.figsize"] = (5,5)  #*define figure size
+#* move
+dx = 1
+dy = 2
+r.move(dx, dy)
 
 #* call display_world and display the robot in it's grid world
-print(r)
 display_world(int(world_size), [r.x, r.y])
+
+
+#*----- ----- -----
+#*   Create Landmark
+#*----- ----- -----
+#* create any number of landmarks
+num_landmarks = 3
+r.make_landmarks(num_landmarks)
+
+#* display the world including these landmarks
+display_world(int(world_size), [r.x, r.y], r.landmarks)
+
